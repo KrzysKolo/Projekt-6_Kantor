da75147aa45835886d395d8beb20b3118fc4e38e
Krzysztofa Kołodziejczak <krzysztofa.kolodziejczak@gmail.com>
2020-07-27 02:25:24 +0200

    Add new files

---
+++ .history/js/index_20200713084154.js
@@ -0,0 +1,19 @@
+const currencyOne = document.querySelector("#currency-one");
+const amountOne = document.querySelector(".amount-one");
+const currencyTwo = document.querySelector("#currency-two");
+const amountTwo = document.querySelector(".amount-two");
+const swapBtn = document.querySelector(".swap");
+const rateInfo = document.querySelector(".rate-info");
+
+const calculate = () => {
+  fetch(
+    `https://api.ratesapi.io/api/latest?base=${currencyOne.value}&symbols=${currencyTwo.value}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      const currency1 = currencyOne.value;
+      const currency2 = currencyTwo.value;
+      const rate = data.rates[currency2];
+    });
+};
+calculate();
